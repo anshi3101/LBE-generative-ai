bb011d60ef11405cf25e62555ec36ea192414f28
Anshi Srivastava <anshisrivastava.284@gmail.com>
2026-07-25 02:54:39 +0530

    Added LangChain_Chains

---
+++ LangChain_Chains/sequential_chains.py
@@ -0,0 +1,28 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template = 'Generate a detailed-report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate a 5 point summary from a following text \n {text}',
+    input_variables=['text']
+)
+
+model = ChatGroq(model='llama-3.1-8b-instant')
+
+parser = StrOutputParser()
+
+chain = prompt1 | model | parser | prompt2 | model | parser 
+
+result = chain.invoke({'topic':'Live your life'})
+
+print(result)
+
+chain.get_graph().print_ascii()
